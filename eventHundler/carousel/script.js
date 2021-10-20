4da6a94ac2a701de86d70a783ed091ce475ee08b
chickenday <92256237+chickenday7@users.noreply.github.com>
2021-10-20 20:21:55 +0300

    Add files via upload

---
+++ eventHundler/carousel/script.js
@@ -0,0 +1,32 @@
+window.onload = init;
+
+let imgArray = ['/JS/mouseonmove/images1.png', '/JS/mouseonmove/images2.png', '/JS/mouseonmove/images3.jpg', '/JS/mouseonmove/images4.png'];
+
+let imgArrayIndex = 0;
+let btn_l = document.getElementById('btn_left');
+let btn_r = document.getElementById('btn_right');
+let images = document.getElementById('collection__images');
+
+function init(){
+   btn_l.onclick = click_left;
+   btn_r.onclick = click_right;
+}
+console.log(imgArray.length);
+function click_left(){
+   imgArrayIndex = imgArrayIndex - 1;
+   if (imgArrayIndex < 0){
+      imgArrayIndex = imgArray.length - 1;
+   }
+   images.src = imgArray[imgArrayIndex];
+
+}
+function click_right(){
+   imgArrayIndex = imgArrayIndex + 1;
+   if (imgArrayIndex > 3){
+      imgArrayIndex = 0;
+   }
+   images.src = imgArray[imgArrayIndex];
+  
+}
+
+console.log(init);
